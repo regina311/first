a0f1236cd7b9725db475d94dbb17ff8861d14f81
regina311 <mr-gorosheka@rambler.ru>
2015-06-24 17:08:50 +0300

    new files
    new files

---
+++ addressbook-selenium-tests/src/com/example/tests/CopyOfGroupCreationTests.java
@@ -0,0 +1,26 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+//import org.testng.annotations.TestInstance;
+
+
+
+
+public class CopyOfGroupCreationTests extends TestBase {
+ 
+	@Test 
+  public void testValidGr() throws Exception {
+	  GroupObject emptyDataGroup = new GroupObject("gyfg", "gdbvg", "vvdv");
+	  createGroup(emptyDataGroup);
+    //validate
+  }
+  
+  @Test
+  public void testGroupWithEmptyDataCanBeCreated() throws Exception {
+	  GroupObject validGroup = new GroupObject("", "", "");
+	  createGroup(validGroup);
+	  //validate
+    
+  }
+}
